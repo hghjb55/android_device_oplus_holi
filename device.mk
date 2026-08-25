935d36fa27da59638c05316432ae952b6f8ba216
hghjb55 <xs_1030@foxmail.com>
2026-08-25 17:29:54 +0800

    更新 device.mk

--- device.mk
+++ device.mk
@@ -12,7 +12,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
-    PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
