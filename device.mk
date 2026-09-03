463ed4586c03caef1189e97853fd312827cf97a4
hghjb55 <168888353+hghjb55@users.noreply.github.com>
2026-09-03 20:18:41 +0800

    更新 device.mk

--- device.mk
+++ device.mk
@@ -5,20 +5,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+LOCAL_PATH := device/oplus/sm6375
+
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+AB_OTA_PARTITIONS ?= system system_ext product vendor vendor_boot vbmeta vbmeta_system vbemta_vendor boot dtbo odm
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.holi
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
