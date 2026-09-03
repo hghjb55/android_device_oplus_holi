89b2ce70420b5db5d3a4c2e988c9ab6810434623
hghjb55 <168888353+hghjb55@users.noreply.github.com>
2026-09-03 21:34:25 +0800

    update

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_sm6375.mk
+    $(LOCAL_DIR)/omni_holi.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_sm6375-user \
-    omni_sm6375-userdebug \
-    omni_sm6375-eng
+    omni_holi-user \
+    omni_holi-userdebug \
+    omni_holi-eng
