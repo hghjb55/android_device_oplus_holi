5490c861f07a28f2b0acc68fba25df5f82cab260
hghjb55 <168888353+hghjb55@users.noreply.github.com>
2026-09-04 18:05:58 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,7 +100,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE      := ext4
 BOARD_SUPER_PARTITION_SIZE              := 9126805504
 BOARD_SUPER_PARTITION_GROUPS            := qti_dynamic_partitions
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE       := 9122611200
-BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm system_dlkm odm
+BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor odm
 BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST += my_bigball my_carrier my_company my_engineering my_heytap my_manifest my_preload my_product my_region my_stock
 
 
